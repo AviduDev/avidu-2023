3b78e3f68287dc62b3302355c873e49637ff6e64
AviduDev <avidudev@gmail.com>
2023-01-15 06:48:50 +0530

    About

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { motion } from "framer-motion";
 import styles from "../../styles/About.module.css";
+import Image from "next/image";
 
 export default function About() {
   const router = useRouter();
@@ -26,6 +27,9 @@ export default function About() {
         />
         <meta property="og:image" content="" />
       </Head>
+
+      {/* ----------------------------NAV---------------------------- */}
+
       <nav className={styles.nav}>
         <button
           className={styles.back}
@@ -38,9 +42,44 @@ export default function About() {
           About
         </motion.h1>
       </nav>
-      <main>
-        <h1>About</h1>
+
+      {/* -----------------------main--------------------------- */}
+
+      <main className={styles.main}>
+        <div className={styles.imageContainer}>
+          <img
+            src="https://res.cloudinary.com/avidu/image/upload/v1672635657/12JOPV7bOKsma6ykaaSmL3tDLV67g4xUQ_uyp0cs.png"
+            height={500}
+          ></img>
+        </div>
+
+        <div className={styles.titleContainer}>
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+            nemo eligendi mollitia ad? Iste doloribus expedita nostrum sequi
+            perferendis quisquam porro corporis nobis? Debitis nam adipisci
+            sapiente fuga nihil ab quas blanditiis est magnam. Officia,
+            laboriosam voluptas exercitationem ab quidem ex, tempora iste
+            ducimus architecto deserunt veniam, vitae beatae odit.
+          </h1>
+        </div>
+
+        <div className={styles.bioContainer}>
+          <div className={styles.bio}>
+            <div className={styles.bioTitle}>
+              <h3>Full Name</h3>
+            </div>
+            <div className={styles.bioDetails}>
+              <p>Avidu Sampath Bandara</p>
+            </div>
+
+          </div>
+
+
+        </div>
       </main>
+
+      {/* ---------------------footer------------------------- */}
     </div>
   );
 }
